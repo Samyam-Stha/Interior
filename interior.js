c93def430270524c29330811619d8ba8746bcc32
Samyam Shrestha <samyamsth2@gmail.com>
2023-11-24 20:26:35 +0545

    FIrst commit

---
+++ interior.js
@@ -0,0 +1,35 @@
+
+
+function openform(){
+    
+    
+ 
+    document.getElementById("info-hide").style.display="none";
+    document.getElementById("head").style.display="none";
+  
+    
+            
+    document.getElementById("form").style.display="block";
+    var container=document.getElementById('background');
+   
+    
+   
+   
+}
+
+    
+
+  function closeform(){
+   
+    
+
+document.getElementById("info-hide").style.display="block";
+    document.getElementById("head").style.display="block";
+    document.getElementById("head").style.display="flex";
+  
+    
+            
+    document.getElementById("form").style.display="none";
+    
+  }
+  
